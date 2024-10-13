f3ea1719171fb65f6f4701d9fbd0ced53e0169c8
safo4eg <stas.rakov111@mail.ru>
2024-10-14 03:14:15 +0700

    feat: добавлена модель и миграция заявки (order)

---
+++ database/migrations/2024_10_13_200437_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedTinyInteger('type'); // продажа/покупка
+            $table->unsignedTinyInteger('asset'); // актив
+            $table->decimal('amount', 38, 18); // количество для обмена
+            $table->unsignedTinyInteger('wallet_type'); // тип кошелька
+            $table->string('wallet_address', 128);
+            $table->decimal('exchange_rate', 10, 2); // курс
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
