03a30086330aef79f87bcb12c1ac4f8552e116d9
safo4eg <stas.rakov111@mail.ru>
2024-10-17 03:41:44 +0700

    feat: добавлены имена для типов кошельков

--- app/Enums/Order/WalletTypesEnum.php
+++ app/Enums/Order/WalletTypesEnum.php
@@ -6,4 +6,12 @@ enum WalletTypesEnum: int
 {
     case BIGMAFIA = 1;
     case EXTERNAL = 2;
+
+    public static function getWalletTypesName(): array
+    {
+        return [
+            self::BIGMAFIA->value => 'Bigmafia BTC',
+            self::EXTERNAL->value => 'Внешний BTC'
+        ];
+    }
 }
