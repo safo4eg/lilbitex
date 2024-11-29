2c163de60186d06b7b4064ed9696ec4c0b0f8867
safo4eg <stas.rakov111@mail.ru>
2024-11-29 23:38:57 +0700

    feat: добавлена команда /order с показом последнего обмена

--- app/Telegram/Commands/User/OrderCommand.php
+++ app/Telegram/Commands/User/OrderCommand.php
@@ -2,7 +2,12 @@
 
 namespace App\Telegram\Commands\User;
 
+use App\Enums\Order\StatusEnum;
+use App\Models\Order;
 use App\Telegram\Conversations\Order\OrderBuyShowMenu;
+use App\Telegram\Conversations\User\CancelledOrderMenu;
+use App\Telegram\Conversations\User\CompletedOrderMenu;
+use App\Telegram\Conversations\User\PendingExchangeOrderMenu;
 use App\Telegram\Conversations\User\PendingPaymentOrderMenu;
 use SergiX44\Nutgram\Handlers\Type\Command;
 use SergiX44\Nutgram\Nutgram;
@@ -15,7 +20,41 @@ class OrderCommand extends Command
 
     public function handle(Nutgram $bot): void
     {
-        // здесь желательно обработку сделать какую из менюшек заказа показывать
-        PendingPaymentOrderMenu::begin(bot: $bot, userId: $bot->userId(), chatId: $bot->chatId());
+        $order = Order::whereHas('user', function ($query) use($bot) {
+            $query->where('chat_id', $bot->userId());
+        })
+            ->latest()
+            ->first();
+
+        switch ($order->status) {
+            case StatusEnum::PENDING_PAYMENT->value:
+                PendingPaymentOrderMenu::begin(
+                    bot: $bot,
+                    userId: $order->user->chat_id,
+                    chatId: $order->user->chat_id
+                );
+                break;
+            case StatusEnum::PENDING_EXCHANGE->value:
+                PendingExchangeOrderMenu::begin(
+                    bot: $bot,
+                    userId: $order->user->chat_id,
+                    chatId: $order->user->chat_id
+                );
+                break;
+            case StatusEnum::COMPLETED->value:
+                CompletedOrderMenu::begin(
+                    bot: $bot,
+                    userId: $order->user->chat_id,
+                    chatId: $order->user->chat_id
+                );
+                break;
+            case StatusEnum::CANCELLED->value:
+                CancelledOrderMenu::begin(
+                    bot: $bot,
+                    userId: $order->user->chat_id,
+                    chatId: $order->user->chat_id
+                );
+                break;
+        }
     }
 }
